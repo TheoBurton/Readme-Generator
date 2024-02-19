5cf7f1b3263ceedd7eff670c9d7dd5a326b14e7c
TheoBurton <111896475+TheoBurton@users.noreply.github.com>
2024-02-19 15:43:09 +0000

    updated the generateMarkdown.js to add the userinput to the readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,39 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// generateMarkdown.js
 
+// Function to generate Markdown content
+function generateMarkdown(data) {
+  return `
+# ${data.title}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This application is covered under the ${data.license} license. ![License Badge](https://img.shields.io/badge/license-${encodeURIComponent(data.license)}-blue.svg)
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For additional questions, you can contact me via GitHub (${data.githubUsername}) or email (${data.email}).
 `;
 }
 
